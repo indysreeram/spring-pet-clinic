4f8ab8e959a85e14ca5c393a7b8464a8df08beec
Sreeram Srinivasaragavan <indysreeram@gmail.com>
2018-11-06 08:19:10 -0500

    Adding POJOs. Closes #2

---
+++ src/main/java/com/sreeram/springpetclinic/model/Person.java
@@ -0,0 +1,15 @@
+package com.sreeram.springpetclinic.model;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/java/com/sreeram/springpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.sreeram.springpetclinic.model;
+
+public class Vet extends Person {
+
+}
