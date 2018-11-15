26ad1ac6e896902c0598fe4bb279088b8dce98ba
Sreeram Srinivasaragavan <indysreeram@gmail.com>
2018-11-15 07:22:10 -0500

    updated Map Impl to create the Id property  ,closes #23

--- pet-clinic-web/src/main/java/com/sreeram/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/sreeram/springpetclinic/bootstrap/DataLoader.java
@@ -23,14 +23,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Sreeram");
         owner1.setLastName("Srinivasaragavan");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Rohan");
         owner2.setLastName("Sreeram");
 
@@ -39,14 +37,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Owners Loaded ....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Pranav");
         vet1.setLastName("Sreeram");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Nivethitha");
         vet2.setLastName("Mathan Mohan");
 
