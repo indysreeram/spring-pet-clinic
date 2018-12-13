dd3b845636062e45e7260da8de9c65c8ec13e02f
Sreeram Srinivasaragavan <indysreeram@gmail.com>
2018-12-13 15:15:12 -0500

    Implemented a Visit map service .closes #46

---
+++ pet-clinic-data/src/main/java/com/sreeram/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.sreeram.springpetclinic.services;
+
+import com.sreeram.springpetclinic.model.Visit;
+
+public interface VisitService  extends CrudService<Visit,Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/sreeram/springpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,47 @@
+package com.sreeram.springpetclinic.services.map;
+
+import com.sreeram.springpetclinic.model.Visit;
+import com.sreeram.springpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit,Long> implements VisitService {
+
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getId() == null || visit.getPet().getOwner() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit Object!!!!");
+        } else {
+            return super.save(visit);
+        }
+
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+}
